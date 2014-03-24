ff30cd55bf6891f3c76334aa5d4600144c80e75f
Charles Jones <cbj3@chuckbjones.com>
2014-03-23 19:10:59 -0500

    Test index route

--- test/app-test.js
+++ test/app-test.js
@@ -8,6 +8,19 @@ var app = require('../app');
 
 describe('General app tests', function(){
 
+  describe('GET /', function(){
+    it('respond with html', function(done){
+      request(app)
+        .get('/')
+        .end(function (res) {
+          res.should.be.html;
+          res.should.have.status(200);
+          res.text.should.match(/Lifecycle/);
+          done();
+        });
+    });
+  });
+
   describe('GET /notexist', function(){
     it('respond with 404', function(done){
       request(app)
