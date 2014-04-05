64c670e014b2da15b4327905f8008b775364d22c
Charles Jones <cbj3@chuckbjones.com>
2014-04-05 11:10:18 -0500

    Add bootstrap styles

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/js/libs', express.static(path.join(__dirname, 'bower_components')));
+app.use('/css/libs', express.static(path.join(__dirname, 'bower_components')));
 app.use(logger.requestLogger);
 
 /// Save off variables needed by all requests
